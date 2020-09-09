208560f9b48836e67285dab6b54f2b0ab648e860
Gabriel Nahum <gnahum12345@gmail.com>
2020-09-08 20:36:06 -0700

    Session 4

---
+++ session 4/InsertLeaf.c
@@ -0,0 +1,12 @@
+struct TreeNode {
+
+  int32_t value;
+  struct TreeNode *left;
+  struct TreeNode *right;
+  struct TreeNode *parent;
+  uint64_t magic;  //not required to modify this.
+}
+
+struct TreeNode * insertNode(struct TreeNode *parent, int val) {
+    // TODO! Implement this! 
+}
